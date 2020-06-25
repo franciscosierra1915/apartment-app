e8bd9a9367f6a3c1ac742a59a9419ab37568ddea
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-06-25 11:33:27 -0500

    done

---
+++ Notes.md
@@ -0,0 +1,25 @@
+Apartment App
+
+(Maybe have an Index with :Apartments and :Tenants?)
+
+
+- [x] See a list of all apartments
+* Click on a apartment to go to the apartment details page
+- [x] See the details of a specific apartment
+* [x]See the apartment's address
+* [x]See a list of the tenants living in the apartment
+- [x] Delete an apartment
+* Then send the user to see the list of all apartments
+- [x] Create a new apartment
+* With it's address
+* After creating the apartment, send the user to see the list of all apartments
+- [x] See a list of all tenants
+* Click on a tenant to go to the tenants edit page
+- [ ] Edit a tenant
+* Change the name of the tenant
+* Change the apartment the tenant lives in
+- [ ] Create a new tenant
+* With their name
+* After creating the tenant, send the user to see the list of all tenants
+- [ ] Delete a tenant
+* Then send the user to see the list of all tenants
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,71 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
-  set :views, "app/views"
+  set :views, "./app/views"
+
+  get('/apartment-list') do 
+    @apartments = Apartment.all
+    erb :apartment_list
+  end
+  get('/get-apartment/:id') do
+      @apartment = Apartment.find(params[:id])
+      erb :one_apartment
+  end
+
+  post('/remove_apartment/:id') do
+    @apartment = Apartment.find(params[:id])
+    @apartment.destroy()
+    redirect('/apartment-list')
+  end
+
+  get('/new-apartment-building') do 
+    @apartment = Apartment.all
+    erb :new_apartment
+  end
+  post('/new-apartment') do 
+    @apartment = Apartment.create({
+        address: params[:apartment_address]
+    })
+    redirect('/apartment-list')
+  end
+
+  get('/tenants-list') do 
+    @tenant = Tenant.all
+    erb :tenants_list
+  end
+  get('/get-tenant/:id') do
+    @apartment = Apartment.all
+    @tenant = Tenant.find(params[:id])
+    erb :one_tenant
+  end
+  post('/remove_tenant/:id') do
+    @tenant= Tenant.find(params[:id])
+    @tenant.destroy()
+    redirect('/tenants-list')
+  end
+
+  get('/new-tenant-create') do
+    @all_apartments =  Apartment.all 
+    @tenant = Tenant.all
+    erb :new_tenant
+  end
+  post('/new-tenant') do 
+    @tenant = Tenant.create({
+        name: params[:tenant_name],
+        apartment_id: params[:apartment_address]
+    })
+    redirect("/get-tenant/#{@tenant.id}") 
+  end
+
+  get("/get-tenant/:id")do
+  @tenant = Tenant.find(params[:id])
+  erb :one_tenant
+  end
+
+  post('/edit-tenant/:id') do
+  @tenant= Tenant.find(params[:id])
+  @tenant.update(name: params[:tenant_name], apartment_id: params[:apartment_address])
+  redirect('/tenants-list')
+  erb :one_tenant
+  end
+
 end
